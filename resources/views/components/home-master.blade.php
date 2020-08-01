cf5c2cfa8672f378e1683ee8d0315901a5e5de47
Dusan <dusantrivic44@hotmail.com>
2020-08-01 19:37:58 +0200

    home-master

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -17,6 +17,16 @@
   <link href="{{asset('css/blog-home.css')}}" rel="stylesheet">
 
    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+     <!-- Custom fonts for this template-->
+  <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}
+  " rel="stylesheet" type="text/css">
+  <link href="{{asset('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i')}}
+  " rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="  {{asset('css/sb-admin-2.css')}}" rel="stylesheet">
+
 </head>
 
 <body>
@@ -151,6 +161,9 @@
   <!-- Bootstrap core JavaScript -->
   <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
   <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+
+
 
 </body>
 
